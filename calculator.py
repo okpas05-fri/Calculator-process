4f6b2ba493679d9f82251f1c0f4eaf6556953231
okpas05-fri <ohiemi4@gmail.com>
2025-09-30 13:05:33 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,21 @@
+
+# num1 = int(input("enter the first number: "))
+# num2 = int(input("enter the second number: "))
+# operator = input("enter an operator +,-,x,/. " )
+# if operator == "+":
+#     result = num1 + num2
+#     print(result)
+# elif operator == "-":
+#     result = num1 - num2  
+#     print(result)   
+# elif operator == "x":
+#     result = num1 * num2
+#     print(result) 
+# elif operator == "/":
+#     result = num1 / num2
+#     print(result)
+# elif operator == "^":
+#     result = num1 ^ num2
+#     print(result) 
+# else:
+#     print("invalid operator")   
